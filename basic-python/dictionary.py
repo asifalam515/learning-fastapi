1b3827eb33924486eca451f22016ec0c28c3d61a
asifalam515 <asibulalam515@gmail.com>
2026-06-12 22:31:19 +0600

    dictionary started

---
+++ basic-python/dictionary.py
@@ -0,0 +1,11 @@
+alien_0 = {'color':'green','point':5}
+for key,value in alien_0.items():
+    print(value)
+
+
+favrtLanguages = {
+    'jen':'c',
+    'asif':'python',
+    'najmul':'c++'
+}
+print(favrtLanguages)
