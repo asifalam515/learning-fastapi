c23b58b59c876882653ec20e6b1891b1db3d58b6
asifalam515 <asibulalam515@gmail.com>
2026-06-14 22:04:11 +0600

    dictionary started

--- basic-python/dictionary.py
+++ basic-python/dictionary.py
@@ -1,11 +1,8 @@
-alien_0 = {'color':'green','point':5}
-for key,value in alien_0.items():
-    print(value)
-
-
-favrtLanguages = {
-    'jen':'c',
-    'asif':'python',
-    'najmul':'c++'
-}
-print(favrtLanguages)
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+for name in favorite_languages.keys():
+    print(name)
