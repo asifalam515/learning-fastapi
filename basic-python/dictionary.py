ad7ed5d16066dadbe30a0b4518ef37c11b8ba807
asifalam515 <asibulalam515@gmail.com>
2026-06-14 22:23:57 +0600

    loop through dictionary

--- basic-python/dictionary.py
+++ basic-python/dictionary.py
@@ -4,5 +4,7 @@ favorite_languages = {
  'edward': 'ruby',
  'phil': 'python',
  }
-for name in favorite_languages.keys():
-    print(name)
+# for name in sorted(favorite_languages.keys()):
+#     print(name.title() + "thank you for talking the pool")
+for val in favorite_languages.values():
+    print(" favourtei language is ",val)
